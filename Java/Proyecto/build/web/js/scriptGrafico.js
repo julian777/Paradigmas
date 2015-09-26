4647e8ae9e435724a7fac433e1aded749c93ba89
julian777 <julian01cs@hotmail.com>
2015-09-26 15:17:34 -0600

    Graficos, extraccion y parseo de datos

--- Java/Proyecto/build/web/js/scriptGrafico.js
+++ Java/Proyecto/build/web/js/scriptGrafico.js
@@ -12,35 +12,52 @@ function init() {
     var datosJson = JSON.parse(datos);
 
     matriz = new Array(datosJson.length);
-    
+
+    for (var i = 0; i < datosJson.length; i++) {
+        var str = JSON.stringify(datosJson[i]);
+        var res = str.split(",");
+
+        res[0] = res[0].replace(patron1, '');
+        res[0] = res[0].replace(patron2, '');
+        res[0] = res[0].replace(patron2, '');
+        res[1] = res[1].replace(patron3, '');
+
+        matriz[i] = new Array(2);
+
+        matriz[i][0] = res[0];
+        matriz[i][1] = res[1];
+
+    }
+
     cargarGrafico();
 
 }
 
 function cargarGrafico() {
-    
+
     var hora = [];
     var cantidad_mensajes = [];
     var tema = [];
-    
-    for(var i = 0; i<matriz.length; i++){
+
+    for (var i = 0; i < matriz.length; i++) {
+        hora[i] = matriz[i][0];
+        cantidad_mensajes[i] = matriz[i][1];
     }
-    
+
     var barChartData = {
-        labels: paises,
+        labels: hora,
         datasets: [
             {
                 fillColor: "#6b9dfa",
                 strokeColor: "#ffffff",
                 highlightFill: "#1864f2",
                 highlightStroke: "#ffffff",
-                data: hora
+                data: cantidad_mensajes
             }
         ]
 
     }
-}
 
     var ctx3 = document.getElementById("chart-area3").getContext("2d");
     window.myPie = new Chart(ctx3).Bar(barChartData, {responsive: true});
-    
+}    
