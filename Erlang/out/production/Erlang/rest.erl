f3c780210132652cc80641df50141ce4667bb93a
alucard43 <mariors094@gmail.com>
2015-09-30 01:48:37 -0600

    rest.erl

---
+++ Erlang/out/production/Erlang/rest.erl
@@ -0,0 +1,31 @@
+%%%-------------------------------------------------------------------
+%%% @author mario
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 29. Sep 2015 10:55 PM
+%%%-------------------------------------------------------------------
+-module(rest).
+-author("mario").
+
+%% API
+-export([post/3]).
+-export([get/1]).
+
+
+get(URL) -> R = get_aux(URL),
+            response_body(R).
+
+post(URL, ContentType, Body) -> R = post_aux(URL,ContentType,Body),
+                                response_body(R).
+
+
+post_aux(URL, ContentType, Body) -> request(post, {URL, [], ContentType, Body}).
+get_aux(URL)                     -> request(get,  {URL, []}).
+
+request(Method, Body) -> httpc:request(Method, Body, [], []).
+
+response_body({ok, { _, _, Body}}) -> Body.
+
+
