fb2de5ba93018845f117c653bfc4e87b953d383b
alucard43 <mariors094@gmail.com>
2015-10-02 11:11:04 -0600

    modelos

---
+++ Java/GestorParadigmas/src/modelo/ModeloFacebook.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import modelo.gestorMensaje.GestorFeedFacebook;
+
+/**
+ *
+ * @author julian
+ */
+public class ModeloFacebook {
+
+    private ModeloFacebook(String servidor) {
+        gUsuarios = new GestorFeedFacebook(servidor);
+    }
+
+    public static ModeloFacebook crearInstancia(String servidor) {
+        mod = new ModeloFacebook(servidor);
+        return mod;
+    }
+
+    public static ModeloFacebook getInstancia() {
+        return mod;
+    }
+
+    public GestorFeedFacebook getgUsuarios() {
+        return gUsuarios;
+    }
+
+    public void setgUsuarios(GestorFeedFacebook gUsuarios) {
+        this.gUsuarios = gUsuarios;
+    }
+
+    private static ModeloFacebook mod = null;
+    private GestorFeedFacebook gUsuarios;
+
+}
