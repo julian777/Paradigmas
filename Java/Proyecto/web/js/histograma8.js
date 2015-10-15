ace22716e117ef5849cf46a069186270a2558bee
julian777 <julian01cs@hotmail.com>
2015-10-14 23:47:13 -0600

    CambiosGraficas3

---
+++ Java/Proyecto/web/js/histograma8.js
@@ -0,0 +1,66 @@
+var vector = [];
+var vector1 = [];
+var vector2 = [];
+function init() {
+
+    var patron1 = "[";
+    var patron2 = '"';
+    var patron3 = "]";
+
+    var datos = document.getElementById("datos").value;
+    var datosJson = JSON.parse(datos);
+    loco = datosJson;
+    for (var i = 0; i < datosJson.length; i++) {
+        st = JSON.stringify(datosJson[i]);
+        var res = st.split(",");
+        
+          
+        res[0] = res[0].replace(patron1, '');
+        res[0] = res[0].replace(patron2, '');
+        res[0] = res[0].replace(patron2, '');
+        res[1] = res[1].replace(patron3, '');
+ 
+        vector1[i] = res[1];
+        vector2[i] = res[0];
+
+    }
+
+    cargarGrafico();
+
+}
+
+function cargarGrafico() {
+
+    var mensaje = [];
+    var cantidad_mensajes = [];
+    
+    for (var i = 0; i < vector1.length; i++) {
+        
+        cantidad_mensajes[i] = vector1[i];
+        mensaje[i] = vector2[i];
+        alert(mensaje[i]);
+         
+    }
+    
+
+    var lineChartData = {
+        labels: mensaje,
+        datasets: [
+            {
+                label: "Primera serie de datos",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "#6b9dfa",
+                pointColor: "#1e45d7",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: cantidad_mensajes
+            }
+        ]
+
+    }
+
+
+    var ctx4 = document.getElementById("chart-area90").getContext("2d");
+    window.myPie = new Chart(ctx4).Line(lineChartData, {responsive: true});
+}
--- Java/Proyecto/web/pag8.jsp
+++ Java/Proyecto/web/pag8.jsp
@@ -1,17 +1,35 @@
-<%-- 
-    Document   : pag8
-    Created on : 07-oct-2015, 19:42:46
-    Author     : julian
---%>
-
+<%@page import="modelo.gestorMensaje.GestorFeedTwitter"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page import="Modelo.ModeloTwitter"%>
+
 <!DOCTYPE html>
-<html>
+<html lang="es">
+
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+        <title></title>
+        <script src="js/Chart.js"></script>
+        <script type="text/javascript" src="js/histograma8.js"></script>
     </head>
-    <body>
-        <h1>Hello World!</h1>
+
+    <% 
+        ModeloTwitter m  = ModeloTwitter.crearInstancia(null); 
+       GestorFeedTwitter gft = m.getgUsuarios();
+        String datos = gft.datosGraficoSieteToJSON();
+    %>
+
+    <body onload="init()">       
+        <h1>Hashtag y su cantidad de apariciones</h1><br>
+        <a href="Twitter.jsp">volver a menu Twitter</a>
+        <br>
+        <a href="pag9.jsp">ir a Grafico siguiente...</a>
+
+        <input id="datos" hidden name="datos" type="text" value='<%=datos%>'/>
+
+
+        <div id="canvas-holder">
+            <canvas id="chart-area90" width="600" height="300"></canvas>
+        </div>
+        <script src="js/histograma8.js"></script>
     </body>
-</html>
+    <html/>
