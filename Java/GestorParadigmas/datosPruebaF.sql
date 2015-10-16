8458e251054045bbf56fe40383f83498bbd56c3e
julian777 <julian01cs@hotmail.com>
2015-10-16 02:43:33 -0600

    datosPruebaF

---
+++ Java/GestorParadigmas/datosPruebaF.sql
@@ -0,0 +1,46 @@
+insert into Facebook values
+('14','#Conciencia','2015-09-01 13:30:40','Heredia',1,'Facebook','Politica','+');
+insert into Facebook values
+('1','#VamosTicos','2015-09-01 13:30:41','Heredia',1,'Facebook','futbol','+');
+insert into Facebook values 
+('2','LuisGuillermo','2015-09-01 13:30:42','Alajuela',0,'Facebook','Politica','-');
+insert into Facebook values
+('3','Era eso todo','2015-09-01 13:30:43','San Jose',0,'Facebook','Desconocido','-');
+insert into Facebook values
+('4','#running','2015-09-01 13:30:44','Limon',1,'Facebook','Politica','=');
+insert into Facebook values
+('5','#running','2015-09-01 13:30:45','Guanacaste',1,'Facebook','Politica','=');
+
+-- futbol
+
+insert into Facebook values
+('5','#running','2015-09-01 18:10:01','Alajuela',1,'Facebook','Politica','+');
+insert into Facebook values
+('6','jajaja como me he reido hoy','2015-09-01 18:10:02','San Jose',0,'Facebook','futbol','+');
+insert into Facebook values
+('4','el deportivo saprissa','2015-09-01 18:10:03','San Jose',0,'Facebook','futbol','-');
+
+-- Educacion
+
+insert into Facebook values
+('5','#running','2015-09-01 20:20:04','Heredia',1,'Facebook','futbol','=');
+insert into Facebook values
+('5','#MEP','2015-09-01 20:21:05','Heredia',1,'Facebook','Educacion','=');
+-- chiste
+insert into Facebook values
+('7','Un dia me rei jaja','2015-09-01 23:40:06','Guanacaste',0,'Facebook','chiste','+');
+-- desconocido
+
+insert into Facebook values
+('7','Caminar es bueno','2015-09-01 16:10:01','Puntarenas',0,'Facebook','desconocido','=');
+insert into Facebook values
+('7','#running','2015-09-01 16:10:02','Limon',1,'Facebook','desconocido','=');
+insert into Facebook values
+('8','#running','2015-09-01 16:10:03','Heredia',1,'Facebook','desconocido','=');
+insert into Facebook values
+('9','En lugar de reir','2015-09-01 16:10:04','Alajuela',0,'Facebook','chiste','-');
+insert into Facebook values
+('10','#running','2015-09-01 16:10:05','Heredia',1,'Facebook','desconocido','-');
+insert into Facebook values
+('12','#running','2015-09-01 16:10:06','Heredia',1,'Facebook','politica','-');
+
