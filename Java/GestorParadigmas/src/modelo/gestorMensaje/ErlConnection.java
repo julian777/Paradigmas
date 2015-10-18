384856697a5f512e28885405069a510f51f8c0a7
julian777 <julian01cs@hotmail.com>
2015-10-17 22:57:49 -0600

    Elbuenojinterface.jar

---
+++ Java/GestorParadigmas/src/modelo/gestorMensaje/ErlConnection.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.gestorMensaje;
+
+import com.ericsson.otp.erlang.OtpConnection;
+import com.ericsson.otp.erlang.OtpErlangObject;
+import com.ericsson.otp.erlang.OtpPeer;
+import com.ericsson.otp.erlang.OtpSelf;
+
+/**
+ *
+ * @author julian
+ */
+
+
+public class ErlConnection {
+
+    private static OtpConnection conn;
+    public OtpErlangObject received;
+    private final String peer;
+    private final String cookie;
+
+    public static void main(String[] args) {
+        new ErlConnection("enode", "erlang");
+    }
+
+    public ErlConnection(String _peer, String _cookie) {
+        peer = _peer;
+        cookie = _cookie;
+        connect();
+
+        /*Do Calls to Rpc methods and then close the connection*/
+        disconnect();
+
+    }
+
+    private void connect() {
+        System.out.print("Please wait, connecting to " + peer + "....\n");
+
+        String javaClient = "java";
+        try {
+            OtpSelf self = new OtpSelf(javaClient, cookie.trim());
+            OtpPeer other = new OtpPeer(peer.trim());
+            conn = self.connect(other);
+            System.out.println("Connection Established with " + peer + "\n");
+        } catch (Exception exp) {
+            System.out.println("connection error is :" + exp.toString());
+            exp.printStackTrace();
+        }
+
+    }
+
+    public void disconnect() {
+        System.out.println("Disconnecting....");
+        if (conn != null) {
+            conn.close();
+        }
+        System.out.println("Successfuly Disconnected");
+    }
+
+}
